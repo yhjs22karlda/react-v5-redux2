403f73feb70417fc0304f7a300e873a817169f35
Daniel Karlsson <yhjs22.karlda@folkuniversitetet.nu>
2023-02-20 15:18:01 +0100

    minimun fuctionallity

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import './App.css'
+import { useDispatch } from "react-redux"
+import { useSelector } from "react-redux"
+import type {state} from "./main.tsx"
+
+export default function App() {
+    const dispatch = useDispatch()
+
+    function handleKeyDown(e) {
+        if(e.key === "Enter") {
+            dispatch(textChange(e.target.value))
+        console.log(e)
+        }
+    }
+    return (
+        <>
+        <div className="app">
+            <input onKeyDown={handleKeyDown} />
+        </div>
+        <Output />
+        </>
+    )
+}
+
+function Output() {
+    let message = useSelector((state:state) => state.name)
+    return <p>Your message: {message}</p>
+}
+
+const textChange = (text:string) => {
+    return {
+        type: "textChange",
+        payload: text
+    }
+}
---
+++ src/main.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {createStore} from "redux"
+import {Provider} from "react-redux"
+import App from './App'
+import './index.css'
+
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+    <Provider store={store}>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </Provider>
+)
+
+export interface state {
+    name:string
+}
+
+interface action {
+    type: string,
+    payload:any
+}
+
+function reducer(state:state={name:""}, action:action):state {
+    switch(action.type) {
+        case "textChange": {
+            return {
+                ...state,
+                name: action.payload
+            }
+        }
+        default:
+            return state
+    }
+}
